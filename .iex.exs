c65fc8b8a398a3ba1c8f3de051fb25ecd16c2a1b
andyl <andy@r210.com>
2026-03-17 09:35:22 -0700

    Refactor spec directories

--- .iex.exs
+++ .iex.exs
@@ -1,6 +1,8 @@
 IO.puts("----- ProGen")
 
-alias ProGen.Script, as: PG
+alias ProGen.Script, as: PS
+alias ProGen.Action, as: PA
+alias ProGen.Validate, as: PV
 
 import_file_if_available("~/.iex.exs")
 
