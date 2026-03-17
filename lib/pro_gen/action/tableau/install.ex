79166cfa02c36945b05bd8f69142e4d1a31b03fe
andyl <andy@r210.com>
2026-03-17 12:19:31 -0700

    Update pseudocode

--- lib/pro_gen/action/tableau/install.ex
+++ lib/pro_gen/action/tableau/install.ex
@@ -3,30 +3,32 @@ defmodule ProGen.Action.Tableau.Install do
   use ProGen.Action
 
   # arguments
-  # - --force-latest - install latest version
-  #
-  #
+  # - --force - force installation of the latest version
 
   @description "Install tableau"
 
-  # add an option for --force
+  # add an option for --
   @option_schema []
 
   def needed?(_args) do
-    # let
-    # if --force == false,
+    # TODO: implement this pseudo code
+    # case {already_installed, force_option} do
+    #   {true, false} -> false
+    #   {true, true}  -> true
+    #   {false, _  }  -> true
+    # end
+
     true
   end
 
   def perform(_args) do
-    # if force_latest == true
-    #   mix archive.install hex tableau_new --force
-    # else
-    #   mix archive.install hex tableau_new
+    ProGen.Sys.cmd("mix archive.install hex tableau_new --force")
     :ok
   end
 
   def confirm(_args) do
+    # Add logic here to confirm that tableau_new is installed.
+    # Maybe confirm by doing 'mix tableau.new --help'
     :ok
   end
 
