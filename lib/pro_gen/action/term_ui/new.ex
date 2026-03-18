71e02a099c87e08071a34bccf14bb5f763077e18
andyl <andy@r210.com>
2026-03-17 17:17:31 -0700

    Add actions

---
+++ lib/pro_gen/action/term_ui/new.ex
@@ -0,0 +1,43 @@
+defmodule ProGen.Action.TermUI.New do
+  @moduledoc """
+  Creates a new TermUI application using the `tableau_new` Mix archive.
+
+  Skips creation when the project directory already exists.
+  Pass `force: true` to regenerate regardless.
+  """
+
+  use ProGen.Action
+  alias ProGen.Sys
+
+  @description "Create a new TermUI application"
+  @option_schema [
+    project: [type: :string, required: true, doc: "Name of the TermUI project to create"]
+  ]
+
+  @impl true
+  def depends_on(_args), do: ["igniter.install"]
+
+  @impl true
+  def needed?(args) do
+    project = Keyword.fetch!(args, :project)
+    not File.dir?(project)
+  end
+
+  @impl true
+  def perform(args) do
+    project = Keyword.fetch!(args, :project)
+    Sys.cmd("rm -rf #{project}")
+    Sys.cmd("mix igniter.new #{project} --install term_ui")
+  end
+
+  @impl true
+  def confirm(_result, args) do
+    project = Keyword.fetch!(args, :project)
+
+    if File.dir?(project) do
+      :ok
+    else
+      {:error, "project directory \"#{project}\" was not created"}
+    end
+  end
+end
