10f51cf7d7eb845591c2e17c9b5c515990bf6a0c
andyl <andy@r210.com>
2026-03-20 15:35:52 -0700

    Tweak UsageRules.Setup

--- lib/pro_gen/action/deps/usage_rules/setup.ex
+++ lib/pro_gen/action/deps/usage_rules/setup.ex
@@ -8,14 +8,14 @@ defmodule ProGen.Action.Deps.UsageRules.Setup do
 
   use ProGen.Action
 
-  alias ProGen.Script, as: PS
-  alias ProGen.CodeMods.MixFile
+  # alias ProGen.CodeMods.MixFile
 
   @validate [{"filesys", [{:has_file, "mix.exs"}]}]
 
   @impl true
   def depends_on(_args) do
-    [{"archive.install", [package: "usage_rules"]}]
+    # [{"archive.install", [package: "usage_rules"]}]
+    []
   end
 
   @impl true
@@ -25,23 +25,27 @@ defmodule ProGen.Action.Deps.UsageRules.Setup do
 
   @impl true
   def perform(_args) do
-    MixFile.add_to_project(:usage_rules, "usage_rules()")
-    MixFile.add_defp(:usage_rules, 0, body())
-    PS.puts("the command 'mix usage_rules.sync' is broken!")
-    PS.puts("see https://github.com/ash-project/usage_rules/issues/62")
-    PS.puts("try running manually...")
+    # these are idempotent...
+    ProGen.CodeMods.MixFile.add_to_project(:usage_rules, "usage_rules()")
+    ProGen.CodeMods.MixFile.add_defp(:usage_rules, 0, body())
+    # always regenerate to sync changes
+    ProGen.Sys.cmd("mix usage_rules.sync --yes")
     :ok
   end
 
   @impl true
   def confirm(_result, _args) do
-    # if File.exists?("RULES.md"), do: :ok, else: {:error, "RULES.md was not created"}
-    :ok
+    if File.exists?("RULES.md") do
+      :ok
+    else
+      {:error, "RULES.md was not created"}
+    end
+    # :ok
   end
 
   # -----
 
-  defp body do
+  def body do
     """
     defp usage_rules do
       [
