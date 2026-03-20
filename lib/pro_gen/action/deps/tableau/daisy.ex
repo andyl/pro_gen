e315e3feff2aa6afd117c689dc39af40cd078600
andyl <andy@r210.com>
2026-03-19 20:16:20 -0700

    Add daisy action

--- lib/pro_gen/action/deps/tableau/daisy.ex
+++ lib/pro_gen/action/deps/tableau/daisy.ex
@@ -13,19 +13,22 @@ defmodule ProGen.Action.Deps.Tableau.Daisy do
 
   use ProGen.Action
 
-  alias ProGen.Script, as: PS
-  alias ProGen.CodeMods.MixFile
+  # alias ProGen.Script, as: PS
+  alias ProGen.CodeMods.File
+
+  @css_file "assets/css/site.css"
 
   @validate [
-    {"filesys", [:has_mix]},
+    {"filesys", [:has_mix, {:has_file, @css_file}]},
     {"mix",     [{:has_dep,  "tableau"}]},
     {"lang",    [:has_npm]},
   ]
 
   @impl true
   def depends_on(_args) do
-    # [{"archive.install", [package: "usage_rules"]}]
-    []
+    [
+      {"npm.install", [package: "daisy@latest"]},
+    ]
   end
 
   # @impl true
@@ -35,11 +38,10 @@ defmodule ProGen.Action.Deps.Tableau.Daisy do
 
   @impl true
   def perform(_args) do
-    MixFile.add_to_project(:usage_rules, "usage_rules()")
-    # MixFile.add_defp(:usage_rules, 0, body())
-    PS.puts("the command 'mix usage_rules.sync' is broken!")
-    PS.puts("see https://github.com/ash-project/usage_rules/issues/62")
-    PS.puts("try running manually...")
+    # add CSS plugin
+    File.append_line(@css_file, ~s(@plugin "daisyui"))
+    # update CSS file
+    File.sed_file(@css_file, ~s(s/\"$/\";/))
     :ok
   end
 
